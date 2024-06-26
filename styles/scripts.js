b39343b79f7d408fb4da54ecc239243b879424b6
Austin Spillman <151957257+Absynthee@users.noreply.github.com>
2024-06-26 14:54:58 +0100

    Updates

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,1 +1,28 @@
-//
+//easter egg
+
+function happyDog() {
+    const elements = document.querySelectorAll('.happydog');
+          elements.forEach(element => {
+              element.classList.add('show');
+          });
+
+          setTimeout(() => {
+              elements.forEach(element => {
+                  element.classList.remove('show');
+              });
+          }, 5000); // 5000 milliseconds = 5 seconds
+      }
+
+      function walkingDog() {
+        const elements = document.querySelectorAll('.walkingdog');
+              elements.forEach(element => {
+                  element.classList.add('show');
+              });
+    
+              setTimeout(() => {
+                  elements.forEach(element => {
+                      element.classList.remove('show');
+                  });
+              }, 5000); // 5000 milliseconds = 5 seconds
+          }
+    
