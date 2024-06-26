31ee0d16db1c21de258fd41538d04f49f44d0810
Austin Spillman <151957257+Absynthee@users.noreply.github.com>
2024-06-26 17:02:24 +0100

    finished
    Updated styles, readme and added screenshot.

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,28 +1,37 @@
-//easter egg
+// window.onload = function () {
+//   const elements = document.querySelectorAll(".phone-element");
+//   let delay = 0;
+
+//   elements.forEach((element) => {
+//     setTimeout(() => {
+//       element.style.opacity = "1";
+//     }, delay);
+//     delay += 1000; // Increase delay for each element
+//   });
+// };
 
 function happyDog() {
-    const elements = document.querySelectorAll('.happydog');
-          elements.forEach(element => {
-              element.classList.add('show');
-          });
+  const elements = document.querySelectorAll(".happydog");
+  elements.forEach((element) => {
+    element.classList.add("show");
+  });
 
-          setTimeout(() => {
-              elements.forEach(element => {
-                  element.classList.remove('show');
-              });
-          }, 5000); // 5000 milliseconds = 5 seconds
-      }
+  setTimeout(() => {
+    elements.forEach((element) => {
+      element.classList.remove("show");
+    });
+  }, 5000);
+}
 
-      function walkingDog() {
-        const elements = document.querySelectorAll('.walkingdog');
-              elements.forEach(element => {
-                  element.classList.add('show');
-              });
-    
-              setTimeout(() => {
-                  elements.forEach(element => {
-                      element.classList.remove('show');
-                  });
-              }, 5000); // 5000 milliseconds = 5 seconds
-          }
-    
+function walkingDog() {
+  const elements = document.querySelectorAll(".walkingdog");
+  elements.forEach((element) => {
+    element.classList.add("show");
+  });
+
+  setTimeout(() => {
+    elements.forEach((element) => {
+      element.classList.remove("show");
+    });
+  }, 5000);
+}
